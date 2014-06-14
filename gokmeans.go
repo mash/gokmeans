e0cccc39375033ea26d0bd466dce2a5f54bbd827
mdesenfants <matt.desenfants@gmail.com>
2014-06-14 01:05:24 -0500

    Updating doc

--- gokmeans.go
+++ gokmeans.go
@@ -1,5 +1,41 @@
-// Gokmeans is a simple k-means clusterer that determines centroids with the Train function,
-// and then classifies additional observations with the Nearest function.
+/*
+Gokmeans is a simple k-means clusterer that determines centroids with the Train function,
+and then classifies additional observations with the Nearest function.
+
+	package main
+
+	import (
+		"fmt"
+		"github.com/mdesenfants/gokmeans"
+	)
+
+	var observations []gokmeans.Node = []gokmeans.Node {
+		gokmeans.Node{20.0, 20.0, 20.0, 20.0},
+		gokmeans.Node{21.0, 21.0, 21.0, 21.0},
+		gokmeans.Node{100.5, 100.5, 100.5, 100.5},
+		gokmeans.Node{50.1, 50.1, 50.1, 50.1},
+		gokmeans.Node{64.2, 64.2, 64.2, 64.2},
+	}
+
+	func main() {
+		// Get a list of centroids and output the values
+		if success, centroids := gokmeans.Train(observations, 2, 50); success {
+			// Show the centroids
+			fmt.Println("The centroids are")
+			for _, centroid := range centroids {
+				fmt.Println(centroid)
+			}
+
+			// Output the clusters
+			fmt.Println("...")
+			for _, observation := range observations {
+				index := gokmeans.Nearest(observation, centroids)
+				fmt.Println(observation, "belongs in cluster", index+1, ".")
+			}
+		}
+	}
+
+*/
 package gokmeans
 
 import (
