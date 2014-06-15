6fbd538b6ce73217d3144ec4182e1ce534a66d39
mdesenfants <matt.desenfants@gmail.com>
2014-06-15 12:21:48 -0500

    Adding example program

---
+++ examples/examples.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mdesenfants/gokmeans"
+)
+
+var observations []gokmeans.Node = []gokmeans.Node {
+	gokmeans.Node{20.0, 20.0, 20.0, 20.0},
+	gokmeans.Node{21.0, 21.0, 21.0, 21.0},
+	gokmeans.Node{100.5, 100.5, 100.5, 100.5},
+	gokmeans.Node{50.1, 50.1, 50.1, 50.1},
+	gokmeans.Node{64.2, 64.2, 64.2, 64.2},
+}
+
+func main() {
+	// Get a list of centroids and output the values
+	if success, centroids := gokmeans.Train(observations, 2, 50); success {
+		// Show the centroids
+		fmt.Println("The centroids are")
+		for _, centroid := range centroids {
+			fmt.Println(centroid)
+		}
+
+		// Output the clusters
+		fmt.Println("...")
+		for _, observation := range observations {
+			index := gokmeans.Nearest(observation, centroids)
+			fmt.Println(observation, "belongs in cluster", index+1, ".")
+		}
+	}
+}
